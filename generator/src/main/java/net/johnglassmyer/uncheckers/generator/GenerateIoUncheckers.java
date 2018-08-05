3751d2bbc8d8317f6a2e5e6e80f2b8da61155ca8
John Glassmyer <john@johnglassmyer.net>
2018-08-05 01:03:00 -0700

    uncheckers: initial commit

---
+++ generator/src/main/java/net/johnglassmyer/uncheckers/generator/GenerateIoUncheckers.java
@@ -0,0 +1,41 @@
+package net.johnglassmyer.uncheckers.generator;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.google.common.base.Charsets;
+
+public class GenerateIoUncheckers {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			throw new RuntimeException("must specify output filename as argument");
+		}
+		Path outputPath = Paths.get(args[0]);
+
+		Class<? extends Exception> checkedExceptionClass = IOException.class;
+		Class<? extends RuntimeException> uncheckedExceptionClass = UncheckedIOException.class;
+
+		String uncheckersPackageName = "net.johnglassmyer.uncheckers";
+		String uncheckersEnclosingClassName = "IoUncheckers";
+		String checkedInterfaceNamePrefix = "CheckedIo";
+		String checkedIntefaceNameSuffix = "";
+		String uncheckerMethodNamePrefix = "uncheck";
+		String uncheckerMethodNameSuffix = "Io";
+
+		String generatedSource = Generator.generate(
+				SamTypes.JAVA_UTIL_FUNCTION_TYPES,
+				checkedExceptionClass,
+				uncheckedExceptionClass,
+				uncheckersPackageName,
+				uncheckersEnclosingClassName,
+				checkedInterfaceNamePrefix,
+				checkedIntefaceNameSuffix,
+				uncheckerMethodNamePrefix,
+				uncheckerMethodNameSuffix);
+
+		Files.createDirectories(outputPath.getParent());
+		Files.write(outputPath, generatedSource.getBytes(Charsets.UTF_8));
+	}
+}
---
+++ generator/src/main/java/net/johnglassmyer/uncheckers/generator/GenerateUncheckers.java
@@ -0,0 +1,40 @@
+package net.johnglassmyer.uncheckers.generator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.google.common.base.Charsets;
+
+public class GenerateUncheckers {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			throw new RuntimeException("must specify output filename as argument");
+		}
+		Path outputPath = Paths.get(args[0]);
+
+		Class<? extends Exception> checkedExceptionClass = Exception.class;
+		Class<? extends RuntimeException> uncheckedExceptionClass = RuntimeException.class;
+
+		String uncheckersPackageName = "net.johnglassmyer.uncheckers";
+		String uncheckersEnclosingClassName = "Uncheckers";
+		String checkedInterfaceNamePrefix = "Checked";
+		String checkedIntefaceNameSuffix = "";
+		String uncheckerMethodNamePrefix = "uncheck";
+		String uncheckerMethodNameSuffix = "";
+
+		String generatedSource = Generator.generate(
+				SamTypes.JAVA_UTIL_FUNCTION_TYPES,
+				checkedExceptionClass,
+				uncheckedExceptionClass,
+				uncheckersPackageName,
+				uncheckersEnclosingClassName,
+				checkedInterfaceNamePrefix,
+				checkedIntefaceNameSuffix,
+				uncheckerMethodNamePrefix,
+				uncheckerMethodNameSuffix);
+
+		Files.createDirectories(outputPath.getParent());
+		Files.write(outputPath, generatedSource.getBytes(Charsets.UTF_8));
+	}
+}
